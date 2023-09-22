07609f40d9eb9027c8a6d5cef865fbfe4c4c2590
Joao Paulo <97894642+Jpasilva1985@users.noreply.github.com>
2023-09-22 15:15:44 +0000

    alteração_treino

---
+++ script.js
@@ -0,0 +1,8 @@
+let sliderElemnt = document.querySelector("#slider");
+let button = document.querySelector("#button");
+let valor = document.querySelector("#valor");
+let senha = document.querySelector("#password");
+let containerPassword = document.querySelector("#container-password");
+
+
+let charset = "abcdefghijlmnopqrstuvxzwy123456789"
