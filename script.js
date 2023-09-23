98061c41f91f8132073c2632ffe78e50d3fd8e83
João Paulo Araújo Da Silva <silvaaraujojp@gmail.com>
2023-09-23 19:49:28 -0300

    treino

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-let sliderElemnt = document.querySelector("#slider");
+let sliderElement = document.querySelector("#slider");
 let button = document.querySelector("#button");
-let valor = document.querySelector("#valor");
+
+let sizePassword = document.querySelector("#valor");
 let senha = document.querySelector("#password");
+
 let containerPassword = document.querySelector("#container-password");
 
 
 let charset = "abcdefghijlmnopqrstuvxzwy123456789"
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.Value;
+
+slider.oninput = function () {
+    sizePassword.innerHTML= this.Value
+}
+
+function generatePassoword() {
+       let pass = "";
+       for(let i = 0, n = charset.length;i< sliderElement.Value;++i){
+            pass += charset.charAt(Math.floor(Math.random() * n))
+       }
+
+   
+       console.log(pass);
+}
